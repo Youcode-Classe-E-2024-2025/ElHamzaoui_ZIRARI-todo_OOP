37fd31841b79d3448e5dd2da9596a64fe0fae733
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-25 18:16:02 +0100

    l'insertion du style du fichier admin.php

--- App/view/admin.php
+++ App/view/admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Interface Admin</title>
-    
+    <link rel="stylesheet" href="asset/admin.css">
 </head>
 <body>
 
