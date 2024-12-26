ff55ed3cdf9181995d4e4b3080e623b9d5fe5a4f
Nada Zirari <zirarinada20@gmail.com>
2024-12-26 09:36:30 +0100

    ajout du code de login

---
+++ App/view/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page de Connexion</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-900 text-gray-200 flex items-center justify-center h-screen">
+  <div class="bg-gray-800 shadow-lg rounded-lg flex w-3/4 max-w-4xl">
+    <!-- Section gauche -->
+    <div class="w-1/2 bg-gray-700 text-gray-200 p-8 flex flex-col justify-center items-center">
+      <img src="logo.png" alt="Logo" class="w-24 h-24 mb-4">
+      <h1 class="text-3xl font-bold mb-2">Bienvenue sur ToDo</h1>
+      <p class="text-center">
+        Organisez vos tâches efficacement et améliorez votre productivité avec notre gestionnaire simple et intuitif.
+      </p>
+    </div>
+   
+   
+</body>
+</html>
