79a6bc71520fbaacbb5a39de783b0673c27249c4
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-29 12:03:28 +0100

    réglement de redirection après l'inscritpion

--- userController.php
+++ userController.php
@@ -13,19 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Créer une instance du modèle pour ajouter la tâche
         $userModel = new UserModel($pdo);
         $userModel->addUser($username, $email, $password,'user');
-        
-        if($email == "example@example.com" && $password == "password123"){
-            $_SESSION['user_id'] = "example@example.com";
-            $_SESSION['user_id'] = "password123";
-            // Rediriger après l'ajout de la tâche
-             header("Location: taskForm.php");
-        }else{
-            $_SESSION['user_id'] = $email['id_user'];
-            $_SESSION['user_id'] = $password['id_user'];
-             // Rediriger après l'ajout de la tâche
              header("Location: user.php");
-        }
-       
+        
+
         exit;
     }
 }
